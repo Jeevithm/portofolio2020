2d3367e8a83bbff5e1c7e6784bc092f6f48fc09a
Jeevith <jeevithm@yahoo.dk>
2020-12-08 18:18:07 +0100

    Updated version

---
+++ js/animation.js
@@ -0,0 +1,38 @@
+
+// Virker i alle browsers
+// Menu behaviour
+let anchorlinks = document.querySelectorAll('a[href^="#"]')
+ 
+for (let item of anchorlinks) {
+    item.addEventListener('click', (e)=> {
+        let hashval = item.getAttribute('href')
+        let target = document.querySelector(hashval)
+        target.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',  
+        })
+        history.pushState(null, null, hashval)
+        e.preventDefault()
+    })
+}
+
+//Go to the top Script
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
---
+++ js/myVideo.js
@@ -0,0 +1,15 @@
+var videoPlayer = document.getElementById("videoPlayer");
+var myVideo = document.getElementById("myVideo");
+
+function stopVideo(){
+    videoPlayer.style.display = "none";
+    myVideo.pause();
+}
+
+function playVideo(file){
+    myVideo.src = file;
+    videoPlayer.style.display = "block";
+    myVideo.autoplay = true;
+    myVideo.load();
+}
+
